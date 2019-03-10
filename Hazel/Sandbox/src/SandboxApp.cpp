244c02617c42596027556fb4734adc41e624a30f
Ya Wang <576330572@qq.com>
2019-03-10 22:15:14 +0800

    Setup basic Application and entry points

---
+++ Hazel/Sandbox/src/SandboxApp.cpp
@@ -0,0 +1,22 @@
+#pragma mark once
+
+#include <stdio.h>
+#include <Hazel.h> 
+
+class Sandbox : public Hazel::Application
+{
+public:
+	Sandbox()
+	{
+
+	}
+	~Sandbox()
+	{
+
+	}
+};
+
+Hazel::Application* Hazel::CreateApplication()
+{
+	return new Sandbox();
+}
